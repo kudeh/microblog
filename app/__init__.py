04b923cc2d7f3c371b8d1b67f37f342a49328435
kudeh <kene.udeh@yahoo.com>
2019-10-05 22:02:27 -0400

    added password reset

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 from config import Config
@@ -20,6 +21,8 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+mail = Mail(app)
 
 
 if not app.debug:
