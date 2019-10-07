8cf70a985f8ca0a67e57fd496e1ef092341ef889
kudeh <kene.udeh@yahoo.com>
2019-10-07 16:38:33 -0400

    added flask-moment features

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 from config import Config
@@ -26,6 +27,8 @@ login.login_view = 'login'
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 
 if not app.debug:
