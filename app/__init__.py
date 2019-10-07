95f0245440ca4e22fd602ce15b26ee72f48f2151
kudeh <kene.udeh@yahoo.com>
2019-10-06 21:16:46 -0400

    added bootstrap styling to templates

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 from config import Config
@@ -23,6 +24,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
